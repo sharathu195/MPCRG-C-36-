7c1128b582ad106ecc5ff08f3881dd727d84330b
Sharath Udupa S <65710185+sharathu195@users.noreply.github.com>
2020-07-20 20:17:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var canvas, BGimg;
+var gameState = 0;
+var database, playerCount;
+var form, player, game;
+var distance = 0;
+var allPlayers;
+
+function setup(){
+    canvas = createCanvas(400,400);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    if(playerCount === 4){
+        game.update(1);
+    }
+    if(gameState === 1){
+        clear();
+        game.play();
+    }
+}
